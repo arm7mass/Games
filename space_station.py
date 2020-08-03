0bbf8d2efc044377106d056903fc0e77aff1dad6
ARM <arm7mass@gmail.com>
2020-08-03 22:29:12 +0300

    update the project

---
+++ space_station.py
@@ -0,0 +1,58 @@
+
+import requests, json, turtle
+
+iss = turtle.Turtle()
+
+def setup(window):
+    global iss
+    window.setup(1000, 500)
+
+    window.bgpic('/Users/arm-pro/DataCenter/Development/books_code/Head_first_Learn_to_code/arm_codes/earth.gif')
+
+    window.setworldcoordinates(-180, -90, 180, 90)
+
+    turtle.register_shape("/Users/arm-pro/DataCenter/Development/books_code/Head_first_Learn_to_code/arm_codes/iss.gif")
+
+    iss.shape("/Users/arm-pro/DataCenter/Development/books_code/Head_first_Learn_to_code/arm_codes/iss.gif")
+
+def move_iss(lat, long):
+    global iss
+    iss.hideturtle()
+    iss.penup()
+    iss.goto(long, lat)
+    iss.pendown()
+    iss.showturtle()
+
+
+def track_iss():
+
+    url = 'http://api.open-notify.org/iss-now.json'
+    response = requests.get(url)
+
+    if (response.status_code == 200):
+        response_dictionary = json.loads(response.text)
+        position = response_dictionary['iss_position']
+        lat = float(position['latitude'])
+        long = float(position['longitude'])
+        move_iss(lat, long)
+    
+    else:
+        print(" Hello Houston , we have aproblem ", response.status_code)
+    widget = turtle.getcanvas()
+    widget.after(5000, track_iss)
+
+
+def main():
+    global iss
+    screen = turtle.Screen()
+    setup(screen)
+    track_iss()
+
+
+if __name__ == "__main__":
+    main()
+    turtle.mainloop()
+
+
+
+
